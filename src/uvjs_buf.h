ec82a57eacd178caa5a0c1e9dc948f0ebb4e07dd
Roman Shtylman <shtylman@gmail.com>
2013-12-01 11:56:09 -0500

    more missing headers

--- src/uvjs_buf.h
+++ src/uvjs_buf.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <assert.h>
+#include <stdlib.h>
+
 #include <uv.h>
 
 namespace uvjs {
--- src/uvjs_fs.h
+++ src/uvjs_fs.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <assert.h>
 #include <string.h>
 
 namespace uvjs {
